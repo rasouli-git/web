dfb31bf767605cd1a925e2eabe088ad04c2e9de2
rasouli-git <mahanrasoulii@yahoo.com>
2022-06-30 20:16:13 +0430

    update

--- basket/assets/js/main.js
+++ basket/assets/js/main.js
@@ -1,12 +1,73 @@
 const shopingCart = document.querySelector('.cart-btn');
 const modal = document.querySelector('.modal');
 const backDrop = document.querySelector('.backdrop');
+const btnConfirm = document.querySelector('.btn.confirm');
+const productDOM = document.querySelector('.product-center');
 
-shopingCart.addEventListener('click',()=>{
+
+import {productsData} from "./product.js";
+// 1. get products
+
+
+class Products{
+
+    getProducts(){
+        return productsData;
+    }
+
+}
+// 2.display product
+class Ui{
+
+    displayProducts(products){
+        let result = '';
+        products.forEach(item => {
+            result +=`<section class="product">
+                            <div class="img-container">
+                                <img class="product-img" src=${item.imageUrl} alt="p -1">
+                            </div>
+                            <div class="product-desc">
+                                <p class="product-title">${item.title}</p>
+                                <p class="product-price">$ ${item.price}</p>
+                            </div>
+                            <button class="add-to-cart" data-id=${item.id} >
+                                add to cart
+                            </button>
+                       </section>`;
+            productDOM.innerHTML=result; 
+        });
+    }
+}
+
+// 3.storage
+
+class Storage{}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    // console.log('loaded');
+    const products = new Products;
+    const productsData = products.getProducts();
+    const ui = new Ui();
+    ui.displayProducts(productsData)
+    // console.log(productsData)
+})
+
+
+shopingCart.addEventListener('click',showModal);
+backDrop.addEventListener('click',closeModal);
+btnConfirm.addEventListener('click',closeModal);
+
+
+
+
+
+function showModal(){
     backDrop.style.display = 'unset';
     modal.style.display = 'unset';
-})
-backDrop.addEventListener('click',()=>{
+}
+
+function closeModal(){
     backDrop.style.display = 'none';
     modal.style.display = 'none';
-})
+
+}
---
+++ basket/assets/js/product.js
@@ -0,0 +1,51 @@
+export const productsData=[
+    {
+        id:1,
+        title: 'product 1',
+        price:99,
+        imageUrl:'./assets/images/product-1.jpeg',
+    },
+    {
+        id:2,
+        title: 'product 2',
+        price:65,
+        imageUrl:'./assets/images/product-2.jpeg',
+    },
+    {
+        id:3,
+        title: 'product 3',
+        price:68,
+        imageUrl:'./assets/images/product-3.jpeg',
+    },
+    {
+        id:4,
+        title: 'product 4',
+        price:98,
+        imageUrl:'./assets/images/product-4.jpeg',
+    },
+    {
+        id:5,
+        title: 'product 5',
+        price:150,
+        imageUrl:'./assets/images/product-5.jpeg',
+    },
+    {
+        id:6,
+        title: 'product 6',
+        price:111,
+        imageUrl:'./assets/images/product-6.jpeg',
+    },
+    {
+        id:7,
+        title: 'product 7',
+        price:48,
+        imageUrl:'./assets/images/product-7.jpeg',
+    },
+    {
+        id:8,
+        title: 'product 8',
+        price:18,
+        imageUrl:'./assets/images/product-8.jpeg',
+    },
+
+]
